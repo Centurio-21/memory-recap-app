9636c31d3cb2d77c4cdd8a37357451c90613368a
Centurio-21 <thiyaguweb2109@gmail.com>
2025-12-29 02:06:53 +0530

    Connect backend to MongoDB Atlas all

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
+require('dotenv').config();
 const express = require('express');
+const connectDB = require('./src/config/db');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
+// Connect Database
+connectDB();
 
 app.get('/', (req, res) => {
-  res.send('Memory Recap Backend is running');
+  res.send('Memory Recap Backend with MongoDB is running');
 });
 
 app.listen(PORT, () => {
